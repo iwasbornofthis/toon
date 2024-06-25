0e22b01937657bcd255657a5aa20bfcffb568641
iwasbornofthis <iwasbornofthis@gmail.com>
2024-06-26 08:38:34 +0900

    6장

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:chapter6/screens/home_screen.dart';
+// import 'package:chapter6/services/api_service.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
